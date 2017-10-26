ff569bf61185ec8564478147106ccae330650eb5
Pablohl <pablohdezlo@hotmail.com>
2017-10-26 14:12:19 +0000

    Creando el método to_s

--- lib/V_energetico/energia.rb
+++ lib/V_energetico/energia.rb
@@ -6,3 +6,20 @@ class Energia
        
         @alimento, @proteinas, @glucidos, @lipidos, @valor_ener = a, b, c, d, []
     end
+    
+    def to_s
+
+        out = "\t\t   Proteínas\tGlúcidos\tLípidos\n"
+        for i in 0..@alimento.size-1 do
+            out << @alimento[i]
+            out << "\t\t"
+            out << @proteinas[i].to_s
+            out << "\t\t"
+            out << @glucidos[i].to_s
+            out << "\t\t"
+            out << @lipidos[i].to_s
+            out << "\n"
+        end
+        out
+    end
+end
