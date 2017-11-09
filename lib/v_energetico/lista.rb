84fbcb7f5b4ac3b7e1c826b4ec8085e7408d6c81
Pablohl <pablohdezlo@hotmail.com>
2017-11-09 18:21:51 +0000

    añadiendo método para extraer

--- lib/v_energetico/lista.rb
+++ lib/v_energetico/lista.rb
@@ -8,3 +8,21 @@ class Lista
         @final= Node.new(nil,nil,nil)
         @size = 0
     end
+    
+    def extract_beg()
+        if(@size==0)
+           puts "No hay elementos"
+        elsif(@size==1)
+           sale = Node.new(@inicio.value,nil,nil)
+           @size -= 1
+           @inicio = Node.new(nil,nil,nil)
+           @final = Node.new(nil,nil,nil)
+           "#{sale.value}"
+        elsif(@size > 1)
+           @size -= 1
+           sale = Node.new(@inicio.value,nil,nil)
+           @inicio.prev.next = nil
+           @inicio = @inicio.prev
+           "#{sale.value}"
+        end
+    end
